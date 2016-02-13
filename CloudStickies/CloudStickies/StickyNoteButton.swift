4aecbea8491418a3278a05426ba3f22083ed6b20
Thiago Magalhães <thiago@macbook-pro-de-thiago.local>
2016-02-13 01:02:54 -0300

    Adding an image for the lock button and adding the configuration button to each note.

---
+++ CloudStickies/CloudStickies/StickyNoteButton.swift
@@ -0,0 +1,13 @@
+//
+//  StickyNoteButton.swift
+//  CloudStickies
+//
+//  Created by Thiago on 13/02/16.
+//  Copyright © 2016 Thiago. All rights reserved.
+//
+
+import Cocoa
+
+class StickyNoteButton: NSButton {
+    
+}
